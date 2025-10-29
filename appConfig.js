3d75605bea02b39ab8363a741220b08b1cb33021
Aleksandr Zagaevskiy <aleksandr.zagaevskiy@axxonsoft.dev>
2025-10-29 23:50:48 +0300

    update health checks

--- appConfig.js
+++ appConfig.js
@@ -10,12 +10,16 @@ require('app-module-path').addPath(path.join(__dirname,'/lib'));
 // Add all routes and route-handlers for your service/app here:
 function serviceRoutes(app) {
 
-  // Add advanced healthcheck middleware (incl. database check)
+  // For Liveness Probe, defaults may be all you need.
+  const livenessCheck = healthcheck({"path" : "/ping"});
+  app.use(livenessCheck.express());
+
+  // For readiness check, let's also test the DB
   const check = healthcheck();
   const AdvancedHealthcheckers = require('healthchecks-advanced');
   const advCheckers = new AdvancedHealthcheckers();
   // Database health check is cached for 10000ms = 10 seconds!
-  check.addCheck('db', 'usersQuery', advCheckers.dbUsersCheck, 
+  check.addCheck('db', 'dbQuery', advCheckers.dbCheck,
     {minCacheMs: 10000});
   app.use(check.express());
 
